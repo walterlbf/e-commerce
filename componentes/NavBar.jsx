26ab6c559f961088305be975ab9e8d0951fafb25
walterlbf <walter.lbf.f@gmail.com>
2021-12-07 22:43:59 -0400

    svg nao renderizando - navBar implementada

--- componentes/NavBar.jsx
+++ componentes/NavBar.jsx
@@ -1,9 +1,21 @@
+import Link from 'next/link';
 import React from 'react';
+import { navLinks } from '../utils/data';
+import logoIcon from '../img/black.svg';
 
 export default function navBar() {
   return (
-    <div>
-      Nav Bar
-    </div>
+    <header>
+      <nav>
+        <img src={logoIcon} alt="logo" />
+        {navLinks.map((link, index) => (
+          <ul>
+            <Link href={link.path}>
+              <li key={index}>{link.name}</li>
+            </Link>
+          </ul>
+        ))}
+      </nav>
+    </header>
   );
 }
---
+++ utils/data.js
@@ -0,0 +1,22 @@
+export const navLinks = [
+  {
+    name: 'Clube',
+    path: '/',
+  },
+  {
+    name: 'Loja',
+    path: '/loja',
+  },
+  {
+    name: 'Produtores',
+    path: '/produtores',
+  },
+  {
+    name: 'Ofertas',
+    path: '/ofertas',
+  },
+  {
+    name: 'Eventos',
+    path: '/eventos',
+  },
+];
